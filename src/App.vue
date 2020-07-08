65964b248ef0a9b798bd06722f3f9dd1c3374a46
zhousaito <zhousaito@163.com>
2020-07-08 20:20:03 +0800

    首页图的一些确定

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
     <div id="app">
-        <h2>Good</h2>
+        <canvas id="canvas"></canvas>
     </div>
 </template>
 
 <script>
+    import hack from "./common/hack";
+    // import rains from "./common/rains";
+
     export default {
         name: 'App',
-        components: {}
+        components: {},
+        mounted() {
+            hack()
+            // rains()
+        }
     }
 </script>
 
 <style>
+    @import "./assets/css/base.css";
 
+    body {
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+    /*#app {*/
+    /*    background-color: black;*/
+    /*}*/
+    canvas {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        background: #000 url("./assets/img/67078.jpg");
+        background-size: cover;
+    }
 </style>
